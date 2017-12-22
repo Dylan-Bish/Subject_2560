cfc59a8a9d31b48153bc38a30dc2b26bd686b73d
Dylan-Bish <dylanbish0717@gmail.com>
2017-12-21 19:15:09 -0500

    Add files via upload
    Added comments, adjusted basic physics values

--- Subject_2560/core/src/com/mygdx/game/GameMain.java
+++ Subject_2560/core/src/com/mygdx/game/GameMain.java
@@ -9,9 +9,12 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 
-import static java.lang.Math.abs;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class GameMain extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -22,8 +25,9 @@ public class GameMain extends ApplicationAdapter {
 	private float charY = 200;
 	private Rectangle still = new Rectangle();
 	private Texture stillImg;
+	private Texture flatTexture;
 	private int moveSpeed = 6;
-	private int jumpSpeed = 10;
+	private int jumpSpeed = 20;
 	private float velocityX = 0;
 	private float velocityY = 0;
 	private float damping_factor;
@@ -31,42 +35,51 @@ public class GameMain extends ApplicationAdapter {
 
 	@Override
 	public void create () {
-
-		batch = new SpriteBatch();
-		testAtlas = new TextureAtlas(Gdx.files.internal("rightroll.Atlas"));
-		rightRollAnimation = new Animation<TextureRegion>(1/30f, testAtlas.getRegions());
-		stillImg = new Texture(Gdx.files.internal("still.png"));
-		damping_factor = 0.07f;
+		batch = new SpriteBatch();		//main spritebatch that's used to draw all of the elements (so far) to the screen
+		testAtlas = new TextureAtlas(Gdx.files.internal("rightroll.Atlas"));	//atlas for main "roll" animation
+		rightRollAnimation = new Animation<TextureRegion>(1/60f, testAtlas.getRegions()); //Actual animation object (60fps)
+		stillImg = new Texture(Gdx.files.internal("still.png"));	//texture for the "still" image (for when the character is moving neither right nor left)
+		flatTexture = new Texture("blackbox.png");	//basic all black texture used to spawn test rectangles
+		damping_factor = 0.07f;									//damping factor for the sliding effect for when the character stops
 	}
 
 	@Override
 	public void render () {
+		//sets background to white
 		Gdx.gl.glClearColor(1, 1, 1, 1);
+		//displays background
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		//these two lines are used to get the time differential needed to display the animation at the correct framerate
 		batch.begin();
 		timePassed += Gdx.graphics.getDeltaTime();
 
-		if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && !jumping)
+		/*
+		Main Key input conditionals
+		 */
+		if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && !jumping)	//if space is pressed and the char is not already jumping
 		{
 			jumping = true;
 			velocityY = 1;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A))
 		{
+			if(velocityX > -1) velocityX -= .15f;
 			batch.draw(rightRollAnimation.getKeyFrame(timePassed, true), charX+100, charY, -100, 100);
-			charX -= moveSpeed;
-			velocityX = -1;
+			charX += moveSpeed*velocityX;
 		}
 		else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D))
 		{
+			if(velocityX < 1) velocityX += .15f;
 			batch.draw(rightRollAnimation.getKeyFrame(timePassed, true), charX, charY, 100, 100);
-			charX += moveSpeed;
-			velocityX = 1;
+			charX += moveSpeed*velocityX;
 		}
 		else
 			batch.draw(stillImg, charX, charY, 100, 100);
 
+		/*
+		main conditionals to handle the very basic "physics" that have so far been implemented
+		 */
 		if(velocityX < 0)
 		{
 			charX += moveSpeed*velocityX;
@@ -78,8 +91,8 @@ public class GameMain extends ApplicationAdapter {
 			velocityX /= (1+damping_factor);
 		}
 		if(jumping) {
-			if (charY < 200) {
-				charY = 200;
+			if (charY < 0) {
+				charY = 0;
 				jumping = false;
 				velocityY = 0;
 			} else {
@@ -88,7 +101,7 @@ public class GameMain extends ApplicationAdapter {
 			}
 		}
 
-
+		//boundary limits so that the the character can never be off-screen
 		if(charX < 0) charX = 0;
 		if(charX > 1180) charX = 1180;
 
@@ -98,6 +111,7 @@ public class GameMain extends ApplicationAdapter {
 
 	@Override
 	public void dispose () {
+		//method to make garbage collection more efficient 
 		batch.dispose();
 		testAtlas.dispose();
 	}
