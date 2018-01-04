6a9700ebae0c8316f2a99219e50b07d30d79594f
Dylan <dylanbish0717@gmail.com>
2018-01-03 19:09:02 -0500

    Added numeric overlay to the health bar and added functionality to the grenade class. Grenades now spawn upon mouseclick in the direction given by the mouse pointer and make the explosion animation.

---
+++ core/assets/maps/Test_Tileset_2.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Damage" tilewidth="180" tileheight="180" tilecount="1" columns="1">
+ <image source="damage.png" width="180" height="180"/>
+ <tile id="0">
+  <properties>
+   <property name="damage" value=""/>
+  </properties>
+ </tile>
+</tileset>
